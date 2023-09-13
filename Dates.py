6ec28c2d40aade449c00a9fc25c0d44c1e5f8fae
Sp0643 <105235131+Sp0643@users.noreply.github.com>
2023-09-13 16:23:10 +0530

    Update Dates.py

--- Dates.py
+++ Dates.py
@@ -1,4 +1,4 @@
-Certainly! You can achieve this by writing a Python code that determines the last working day based on the current day of the week. Here's a sample code to do that:
+kiCertainly! You can achieve this by writing a Python code that determines the last working day based on the current day of the week. Here's a sample code to do that:
 
 ```python
 import datetime
@@ -100,3 +100,62 @@ print("Last working day:", last_working_day)
 ```
 
 This code defines a function `find_last_working_day` that takes a date and iterates backward until it finds a weekday (Monday to Friday). You can change the `current_date` variable to any date you want, and it will return the last working day for that date.
+
+
+
+
+Certainly! You can use Python to download files using Selenium and check if the downloaded file already exists in a folder. If it doesn't exist, you can proceed with the download. Here's an example of how you can achieve this:
+
+```python
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Define the folder where you want to save the downloaded files
+download_folder = '/path/to/your/download/folder/'
+
+# Set up Chrome WebDriver with download options
+chrome_options = Options()
+chrome_options.add_experimental_option('prefs', {
+    'download.default_directory': download_folder,
+    'download.prompt_for_download': False,
+    'download.directory_upgrade': True,
+    'safebrowsing.enabled': False,
+    'safebrowsing.disable_download_protection': True
+})
+
+# Initialize the WebDriver
+chromedriver_path = '/path/to/chromedriver'
+driver = webdriver.Chrome(executable_path=chromedriver_path, options=chrome_options)
+
+# Navigate to the webpage where the file download link is
+driver.get('https://example.com')
+
+# Locate and click the download link (you'll need to adjust this based on your webpage)
+download_link = driver.find_element(By.XPATH, '//*[@id="downloadButton"]')
+download_link.click()
+
+# Wait for the download to complete (you can adjust the timeout as needed)
+wait = WebDriverWait(driver, 60)
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="downloadCompleteIndicator"]')))
+
+# List files in the download folder
+downloaded_files = os.listdir(download_folder)
+
+# Check if the file you want to download is not already in the folder
+file_to_download = 'file_to_download.zip'
+if file_to_download not in downloaded_files:
+    print(f"Downloading {file_to_download}")
+    # Perform the download here
+
+# Close the WebDriver
+driver.quit()
+```
+
+In this code, replace `'/path/to/your/download/folder/'` with the path to the folder where you want to save the downloaded files, and adjust the URL and element location code to match your specific webpage.
+
+This code first checks if the file you want to download already exists in the specified folder. If it doesn't, it initiates the download. This way, you can avoid downloading duplicate files.
